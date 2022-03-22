ad4b9ffdf724e4deee51858f6bf2114f4a0474b4
elmehdi rhindi <g6handi@gmail.com>
2022-03-22 20:39:05 +0100

    edit route for the post

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -16,11 +16,29 @@ router.post('/', async (req, res) => {
   res.send(post)
 })
 
-// dete a post using the post id
+// Get one post using the post id
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const post = await Post.findById(id)
+  res.send(post)
+})
+
+// delete a post using the post id
 router.delete('/:id', async (req, res) => {
   const { id } = req.params
   await Post.findByIdAndDelete(id)
   res.send(`post with id ${id} has been deleted`)
+})
+
+// edit all attributes of the post (put)
+router.put('/:id', async (req, res) => {
+  const { title, content } = req.body
+  const { id } = req.params
+  const post = await Post.findById(id)
+  post.title = title
+  post.content = content
+  await post.save()
+  res.send(post)
 })
 
 module.exports = router
