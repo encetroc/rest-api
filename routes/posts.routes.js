430a182121d2229f7aa7f684c1e768834c52f12e
elmehdi rhindi <g6handi@gmail.com>
2022-03-22 19:44:37 +0100

    create new post route

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const Post = require('../models/post.model')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
   res.send(['post1', 'post2'])
+})
+
+// create a new post
+router.post('/', async (req, res) => {
+  const { title, content } = req.body
+  const post = await Post.create({ title, content })
+  res.send(post)
 })
 
 module.exports = router
